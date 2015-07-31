0525fc4962711d79e83cbaff63cf36e11ea453b4
tarcisse <tarcisses@yahoo.fr>
2015-07-31 14:55:48 +0200

    site vitrine actuelle
    je remis le site actuelle sur git

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
 
+//die($_SERVER['REMOTE_ADDR']);
+
+
+if(isset($_GET['vendre']))
+{
+   if($_GET['vendre']=='du reve')
+   {
+    
+   }
+   else{
+    header('location:http://soulnbeauty.be/progress/index.html');
+   }
+}
+else
+{
+     //die("authentication failed  your ip is:".$_SERVER['REMOTE_ADDR']);
+     header('location:http://soulnbeauty.be/progress/index.html');
+}
 // On charge le framework Silex
 require_once 'vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
 
 // On définit des noms utiles
 use Silex\Application;
@@ -10,15 +29,28 @@ use Silex\Application;
 $app = new Application();
 $app['debug'] = true;
 
-/*
+ /*
  *inclusion de module
  */
+
+////------------------------session---------------------------------------
+$app->register(new Silex\Provider\SessionServiceProvider());
+if(isset($_GET['lang']))
+{
+    $app['session']->set('lang',$_GET['lang']);
+    
+}
+
+if(! $app['session']->get('lang'))
+{
+   $app['session']->set('lang','fr');
+}
 
 ////----------------twig-----------------------------------------------------
 $app->register(new Silex\Provider\TwigServiceProvider(), 
 	       array('twig.path' => '.',));
 $app->register(new Silex\Provider\TranslationServiceProvider(), array(
-    'locale_fallbacks' => array('fr'),
+    'locale_fallbacks' => array( $app['session']->get('lang')),
 ));
 ////----------------translation-----------------------------------------------------
 use Symfony\Component\Translation\Loader\YamlFileLoader;
@@ -27,21 +59,21 @@ $app['translator'] = $app->share($app->extend('translator', function($translator
     $translator->addLoader('yaml', new YamlFileLoader());
 
     $translator->addResource('yaml', __DIR__.'/locales/en.yml', 'en');
-    $translator->addResource('yaml', __DIR__.'/locales/do.yml', 'do');
+    $translator->addResource('yaml', __DIR__.'/locales/nl.yml', 'nl');
     $translator->addResource('yaml', __DIR__.'/locales/fr.yml', 'fr');
 
     return $translator;
 }));
+////----------------------------variable global------------------
+$page=array();
+$page['titre']='';
+$page['description']='';
+$page['key']='';
 
-// Maintenant on démontre l'utilisation des templates Twig
-// On commence par charger le moteur de templating
-$app->register(new Silex\Provider\TwigServiceProvider(), 
-	       array('twig.path' => '.',));
 
 /*
  *fin inclusion module
  */
-
  
  
  
@@ -51,24 +83,45 @@ $app->register(new Silex\Provider\TwigServiceProvider(),
  //                                                                                                                                      //
  /*--------------------------------------------------------------------------------------------------------------------------------------*/
 // On définit une route pour l'url /
-$app->get('/', function(Application $app) {
-   $titre='Accueil';
-    return $app['twig']->render('web/index.html', array('title' => $titre));
-  });
-$app->get('/test', function(Application $app) {
-   $titre='Accueil';
-    return $app['twig']->render('web/test2.html', array('title' => $titre));
-  });
-
-
-
-// On définit une route qui répond à tout url de la forme /blabla
-// en répondant Hello blabla
-$app->get('/{nomuser}', function(Application $app,$nomuser) {
-  
-  return $nomuser;
-  });
-
+$app->get('/', function(Application $app , Request $req) {
+    global $page;
+    $page['titre']='SOUL & BEAUTY';
+    $page['description']='ACCUEIL';
+    $page['key']='';
+    if($req->query->get('scroll'))
+       {
+	$page['scroll']=$req->query->get('scroll');
+       }
+    
+  //return print_r($page);
+    return $app['twig']->render('index.html',array('page'=>$page));
+});
+ 
+ /*
+  *photo url
+  *
+  */
+ $app->get('/photo', function(Application $app) {
+    global $page;
+    $page['titre']='PHOTOS';
+    $page['description']='';
+    $page['key']='';
+    
+    return $app['twig']->render('photo.html',array('page'=>$page));
+});
+ /*
+  *videos
+  *
+  */
+ $app->get('/video', function(Application $app) {
+    global $page;
+    $page['titre']='VIDEOS';
+    $page['description']='';
+    $page['key']='';
+    
+    return $app['twig']->render('video.html',array('page'=>$page));
+});
+ 
 // On lance l'application
 $app->run();
 
--- sqlbuddy/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-curl -sS https://getcomposer.org/installer | php
-php composer.phar install
